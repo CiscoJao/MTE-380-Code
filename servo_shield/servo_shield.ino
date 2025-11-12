15e4da466adacc62bdeeb6fc5993542dfa7785da
DanielKang747 <danielkang747@gmail.com>
2025-11-12 11:46:28 -0500

    converted_angle_into_PWM

--- servo_shield/servo_shield.ino
+++ servo_shield/servo_shield.ino
@@ -8,10 +8,13 @@ static const int SERVO_MAX = 500; // CCW
 static const int SERVO_MIN = 100; // CW
 static const int SERVO_MID = (SERVO_MAX + SERVO_MIN) / 2;
 static const int PWM_FREQ = 50;
+
+int targetPWM = SERVO_MID;
+bool newCommand = false;
+
 int x = 0;
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
 
 void setup() {
     Serial.begin(9600);
@@ -27,8 +30,22 @@ void setup() {
 }
 
 void loop() {
-    // move all servos once
-    pwm.setPWM(SERVO1, 0, SERVO_MID);
-    pwm.setPWM(SERVO2, 0, SERVO_MID);
-    pwm.setPWM(SERVO3, 0, SERVO_MID + x);
+
+  if (Serial.available() > 0) {
+    // Read the incoming byte
+    int receivedPWM = Serial.read();
+
+    // Validate PWM range
+    if (receivedPWM >= SERVO_MIN && receivedPWM <= SERVO_MAX) {
+      targetPWM = receivedPWM;
+      newCommand = true;
+    }
+  }
+
+  if (newCommand) {
+        // move all servos once
+    pwm.setPWM(SERVO1, 0, targetPWM);
+    // pwm.setPWM(SERVO2, 0, targetPWM);
+    // pwm.setPWM(SERVO3, 0, targetPWM + x);
+  }
 }
