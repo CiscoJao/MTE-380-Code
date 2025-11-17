b74a046029d5fbcdbe8d8c61f1e92ad2d8d49697
CiscoJao <ciscojao@icloud.com>
2025-11-17 17:06:17 -0500

    started main arduino code

---
+++ stewart_platform/stewart_platform.ino
@@ -0,0 +1,55 @@
+#include <Adafruit_PWMServoDriver.h>
+
+#define SERVO1   0
+#define SERVO2   4
+#define SERVO3   8
+
+static const int ARM_MAX = 200; // physical upper limit of the arms
+static const int SERVO_MAX = 500; // CCW 180 deg
+static const int SERVO_MIN = 100; // CW 0 deg
+static const int SERVO_MID = (ARM_MAX + SERVO_MIN) / 2;
+static const int PWM_FREQ = 50;
+int x = 100; // for testing, delete later
+int targetAngle[3] = {0, 0, 0};
+bool newCommand = false;
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void setup() {
+    Serial.begin(9600);
+
+    pwm.begin();
+    pwm.setPWMFreq(PWM_FREQ);
+
+    // move all servos to zero position
+    // pwm.setPWM(SERVO1, 0, SERVO_MID);
+    // pwm.setPWM(SERVO2, 0, SERVO_MID);
+    // pwm.setPWM(SERVO3, 0, SERVO_MID);
+    // delay(1000);
+}
+
+void loop() {
+
+    // read target angles from serial
+    if (Serial.available() > 0) {
+        int receivedAngle = Serial.read();
+
+        // clip the angle for safety
+        if (receivedAngle >= SERVO_MIN && receivedAngle <= ARM_MAX) {
+            targetAngle[0] = receivedAngle;
+            targetAngle[1] = 0; // change later
+            targetAngle[2] = 0; // change later
+            newCommand = true;
+        }
+    }
+
+    // update target angles for the servos
+    if (newCommand) {
+        pwm.setPWM(SERVO1, 0, targetAngle[0]);
+        pwm.setPWM(SERVO2, 0, targetAngle[1]);
+        pwm.setPWM(SERVO3, 0, targetAngle[2]);
+        newCommand = false;
+    }
+
+    delay(10);
+}
